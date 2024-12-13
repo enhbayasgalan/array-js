151d4825f26cdbb0f07cac3b33f440b1dc2b2dd3
enhbayasgalan <enhuush0109@gmail.com>
2024-12-13 10:35:01 +0800

    w

--- index.js
+++ index.js
@@ -191,69 +191,81 @@ const originalFlavors = [
 // 1. write function ==> function will take array as input
 // 2. sort the numbers inside the array and return sorted array
  
-function sortedArray(array) {}
- 
+function sortedArray(array) {
+  var temp = 0;
+  for (var i = 0; i < array.length; i++) {
+    for (var j = i; j < array.length; j++) {
+      if (array[j] < array[i]) {
+        temp = array[j];
+        array[j] = array[i];
+        array[i] = temp;
+      }
+    }
+  }
+  return array;
+}
+
 console.log(sortedArray([5, 7, 10, -10, 56]));
 // ==> [56, 10, 7,5,-10]
-/*
+
+
+// Exercise 1
  
+// Create a function that will display the smallest value in the array.
  
- `
-Exercise 1
+// Example:
+// > console.log(findSmallest([30, 45, 60, 7]));
+// > 1
+
+// Exercise 2
  
-Create a function that will display the smallest value in the array.
+// Create a function that will accept an array, check the data type of each element. The function will delete string elements and will return a the new array
  
-Example:
-> console.log(findSmallest([30, 45, 60, 7]));
-> 1
+// Example:
+// > console.log(numbersOnly(['text', 3, 7, 'github', 13, 'dev']));
+// > [ 3, 7, 13 ]
+let array = ['text', 3, 7, 'github', 13, 'dev'];
+for (let i = array.length - 1; i >= 0; i--) {
+  if (typeof array[i] === "string") {
+    array.splice(i, 1);
+  }
+}
+
+// console.log(array);
+
+// Exercise 3
  
+// Create a function that will accept an array and do the following:
+// Get the lowest element
+// Get the highest element
+// Get the length of array
+// Get the Average of all element;
+// Store these criteria in a new array
  
-Exercise 2
+// Example:
+// > console.log(minMaxLengthAverage([7, 13, 3, 77, 100]));
+// > [ 3, 100, 5, 40 ]\
+
+// Exercise 4
  
-Create a function that will accept an array, check the data type of each element. The function will delete string elements and will return a the new array
+// Count the number of Words
+// Return how many words was given
  
-Example:
-> console.log(numbersOnly(['text', 3, 7, 'github', 13, 'dev']));
-> [ 3, 7, 13 ]
- 
-Exercise 3
- 
-Create a function that will accept an array and do the following:
-Get the lowest element
-Get the highest element
-Get the length of array
-Get the Average of all element;
-Store these criteria in a new array
- 
-Example:
-> console.log(minMaxLengthAverage([7, 13, 3, 77, 100]));
-> [ 3, 100, 5, 40 ]
- 
-Exercise 4
- 
-Count the number of Words
-Return how many words was given
- 
-Example:
-> countWords('hello from kbpsystem!');
-> 3
- 
-Exercise 5
- 
-Multiply by Length
-Multiply all elements in an array by it's length
- 
-Example:
-> MultiplyByLength([4,1,1]);
-> [12, 3, 3]
- 
-Exercise 6
- 
-Find the correct Index location
-Return the index location of an element from a given array. First argument is the array you'd like to search and the second one is the element (either string/number) to look for.
- 
-Example:
-> console.log(findIndex(['github', 'gitlab', 'bitbucket', 'apollo'], 'gitlab'));
-> 1
- 
-*/
+// Example:
+// > countWords('hello from kbpsystem!');
+// > 3
+function countWords(sentence) {
+  let count = 1;
+
+  for (let char of sentence) {
+      if (char === ' ') {
+          count++;
+      }
+  }
+
+  return count;
+}
+
+
+let sentence = 'hello from kbpsystem!';
+console.log("Number of words:", countWords(sentence));
