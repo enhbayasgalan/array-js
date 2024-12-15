c4d726acefe9d338b951c531e63ceca7ca648b3b
enhbayasgalan <enhuush0109@gmail.com>
2024-12-15 13:18:17 +0800

    p

--- index.js
+++ index.js
@@ -454,3 +454,370 @@ function greeting(firstName, lastName) {
   return `Hello ${firstName} ${lastName}, nice to meet you!`;
 }
 console.log(greeting(`Hello, nice to meet you!`))
+
+/* Step 3: Check your work by un-commenting the following calls to consume(): */
+// 4
+// 160 // Hello Mary Poppins, nice to meet you!
+
+
+ 
+
+// ///////////////Menu Items (MVP)///////////////////
+ 
+// const latte = { name: "Cafe Latte", price: 4, category: "Drinks" };
+// const burger = { name: "Burger", price: 18, category: "Lunch" };
+// const breakfastBurrito = {
+//   name: "Breakfast Burrito",
+//   price: 16,
+//   category: "Breakfast",
+// };
+ 
+/* Task 1a: write a function to return more menu items with the same format as the items above. */
+ 
+function createMenuItem(name, cost, category) {
+  return { name: name, price: cost, category: category };
+}
+
+/* Task 1b: use your function to create 3 more menu items. You may add any items to the menu that you'd like */
+const coffee = createMenuItem("Americano", 3.5, "Drinks");
+const salad = createMenuItem("Caesar Salad", 12, "Lunch");
+const smoothie = createMenuItem("Mango Smoothie", 5, "Drinks");
+
+/* Task 2: You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to your burger object that automatically calculates price given a string as a parameter.
+Your method should accept:
+(1) A string (teacher, student, or public)
+and should return a number.
+For example, burger.discount("teacher") would return 13.5 and burger.discount("public") would return 16.2*/
+const burger = {
+  name: "Burger",
+  price: 18,
+  category: "Lunch",
+  discount: function(customerType) {
+    let discountPercentage;
+    if (customerType === "teacher" || customerType === "student") {
+      discountPercentage = 0.25; // 25% discount for teachers and students
+    } else if (customerType === "public") {
+      discountPercentage = 0.10; // 10% discount for the public
+    } else {
+      return "Invalid customer type"; // If the customer type is invalid
+    }
+
+    return this.price - (this.price * discountPercentage);
+  }
+};
+
+// Example usage
+console.log(burger.discount("teacher"));  // Outputs: 13.5
+console.log(burger.discount("student"));  // Outputs: 13.5
+console.log(burger.discount("public"));   // Outputs: 16.2
+
+
+
+///////////////Reviews (MVP)///////////////////
+ 
+const reviews = [
+  {
+    name: "Daniela",
+    rating: 5,
+    feedback: "Beautiful atmosphere and wonderful vegan options!",
+  },
+  {
+    name: "Jack",
+    rating: 3,
+    feedback:
+      "A little too hipster for my taste, but the burger was decent, if overpriced",
+  },
+  { name: "Miranda", rating: 4, feedback: "fun trivia and cool vibes" },
+  {
+    name: "Wen",
+    rating: 4.5,
+    feedback:
+      "I don't leave my house often, but when I do, it's for this place. Highly reccomend.",
+  },
+  {
+    name: "Brett",
+    rating: 3,
+    feedback:
+      "great selection of snacks and a nice cafe area to get work done during the day.",
+  },
+  {
+    name: "Julius",
+    rating: 2,
+    feedback:
+      "I was largely unimpressed by this venue. Nothing special on the menu and too expensive. The atmosphere is polarizing, and not for me, but I think some would like it.",
+  },
+  {
+    name: "Lauren",
+    rating: 4,
+    feedback:
+      "Absolutely love that they have karaoke Fridays! Food and drink selection is okay.",
+  },
+  { name: "Reyna", rating: 3.5, feedback: "" },
+];
+ 
+/* Task 3: Console.log just Julius' feedback */
+// console.log(reviews[5].feedback);  
+
+/* Task 4: Add a new rating with your (fictitious) opinions of the restaurant in the same format as the reviews above. */
+// reviews.push({
+//   name: "Alex",
+//   rating: 4.5,
+//   feedback: "Great place for hanging out with friends, food is solid but could use more variety.",
+// });
+
+// /* Task 5: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
+// reviews[6].feedback = "This place is chill with really cool people, great for getting work done on weekdays.";  
+
+/*  Task 6: Write a function to return a review based on the index of the review in the array.
+Your function should take two arguments:
+(1) an array which holds all of the reviews
+(2) a number which is the desired index in the array.
+and should return a string in the format `{name} gave the restaurant a {rating}, and their feedback was: {feedback}`
+*
+* For example, if getReviewByIndex is invoked with reviews and the number 0
+* it will return `Daniela gave the restaurant a 5 star review and their feedback was: Beautiful atmosphere and wonderful vegan options!`
+*/
+// function getReviewByIndex(reviews, index) {
+//   const review = reviews[index];
+//   return `${review.name} gave the restaurant a ${review.rating} star review and their feedback was: ${review.feedback}`;
+// }
+
+// /* Exercise 12
+ 
+// Create the use object and the object should have following key
+// firstName
+// lastName
+// currentLocation
+// favoriteSongs
+// age
+ 
+// 1. console log 'hello user's firstname and Lastname'
+// 2.How's the weather in 'currentLocation'
+// 3. console log user's favorite songs
+// Step 1: Create the user object
+const user = {
+  firstName: 'John',
+  lastName: 'Doe',
+  currentLocation: 'New York',
+  favoriteSongs: ['Song 1', 'Song 2', 'Song 3'],
+  age: 30
+};
+
+console.log(`Hello, ${user.firstName} ${user.lastName}!`);
+
+console.log(`How's the weather in ${user.currentLocation}?`);
+
+console.log("User's favorite songs are:");
+user.favoriteSongs.forEach(song => console.log(song));
+
+
+/* STRETCH 3:  This challenge is not related to the data above!
+Write a function called carMarker
+Your function should accept:
+(1) a single odometer argument (a number)
+and return an object.
+The returned object should have the following characteristics:
+     it has an `odometer` property that contains the argument passed in.
+     it has a `drive` method that takes a distance as its argument, and
+         (1) causes the odometer in the object to be increased by the distance,
+         (2) returns the updated value of the `odometer`.
+*/
+ 
+/* STRETCH 3:  This challenge is not related to the data above!
+Write a function called carMarker
+Your function should accept:
+(1) a single odometer argument (a number)
+and return an object.
+The returned object should have the following characteristics:
+     it has an `odometer` property that contains the argument passed in.
+     it has a `drive` method that takes a distance as its argument, and
+         (1) causes the odometer in the object to be increased by the distance,
+         (2) returns the updated value of the `odometer`.
+*/
+ 
+function carMarker(odometer) {
+  return {
+    odometer: odometer, 
+    drive: function(distance) { 
+      this.odometer += distance;
+      return this.odometer; 
+    }
+  };
+}
+
+
+// Exercise 13
+ 
+let superChimpOne = {
+  name: "Chad",
+  species: "Chimpanzee",
+  mass: 9,
+  age: 6,
+  astronautID: 1,
+  move: move(),
+};
+let salamander = {
+  name: "Lacey",
+  species: "Axolotl Salamander",
+  mass: 0.1,
+  age: 5,
+  astronautID: 10,
+  move: move(),
+};
+let animal1 = {
+  name: "Brad",
+  species: "Chimpanzee",
+  mass: 11,
+  age: 6,
+  astronautID: 5,
+  move: move(),
+};
+let animal2 = {
+  name: "Leroy",
+  species: "Beagle",
+  mass: 14,
+  age: 5,
+  astronautID: 7,
+  move: move(),
+};
+let animal3 = {
+  name: "Almina",
+  species: "Tardigrade",
+  mass: 0.0000000001,
+  age: 1,
+  astronautID: 3,
+  move: move(),
+};
+function move() {
+  return Math.round(Math.random() * 10);
+}
+// After you have created the other object literals, add the astronautID property to each one.
+//Create an array to hold the animal objects.
+let crew = [superChimpOne, salamander, animal1, animal2, animal3];
+// Print out the relevant information about each animal.
+// return `${name} is a ${species}. They are ${age} years old and ${mass} kilograms. Their ID is ${astronautID}.`;
+function crewReports(animal) {
+  return `${animal.name} is a ${animal.species}. They are ${animal.age} years old and ${animal.mass} kilograms. Their ID is ${animal.astronautID}.`;
+}
+crew.forEach(animal => {
+  console.log(crewReports(animal));
+});
+
+
+// Exercise 14
+ 
+var movieDatabase = {
+  title: "Pulp Fiction",
+  duration: 120,
+  stars: ["Bruce Willis", "Uma Thurman"],
+};
+// write the function to return Movie name and duration and start
+function getMovieDetails(movie) {
+  return `Movie: ${movie.title}, Duration: ${movie.duration} minutes, Stars: ${movie.stars.join(", ")}`;
+}
+console.log(getMovieDetails(movieDatabase));
+// Create an object to hold information on your favorite recipe. It should have properties for title (a string), servings (a number), and ingredients (an array of strings).
+const recipe = {
+  title: "Mole",
+  servings: 2,
+  ingredients: ["cumin", "cinnamon", "cocoa"],
+};
+console.log("Recipe Title:", recipe.title);
+console.log("Servings:", recipe.servings);
+console.log("Ingredients:");
+recipe.ingredients.forEach(ingredient => {
+  console.log(ingredient);
+})
+// console.log title of the recipe
+// console.log 'Servings: recipe servings'
+//console.log the ingredients one by one
+
+// //Task2
+ 
+// let programming = {
+//   languages: ["JavaScript", "Python", "Ruby"],
+//   isChallenging: true,
+//   isRewarding: true,
+//   difficulty: 8,
+//   jokes:
+//     "http://stackoverflow.com/questions/234075/what-is-your-best-programmer-joke",
+// };
+ 
+// Write the command to add the language "Go" to the end of the languages array.
+// programming.languages.push("Go");
+// Change the difficulty to the value of 7
+// Using the delete keyword, write the command to remove the jokes key from the programming object.
+// delete programming.jokes;
+// Write the command to add a new key called isFun and a value of true to the programming object.
+// programming.isFun = true;
+// Using a loop, iterate through the languages array and console.log all of the languages.
+// for (let i = 0; i < programmi.languages.length; i++) {
+//   console.log(programming.languages[i]);
+// }
+// Using a loop, console.log all of the keys in the programming object.
+// for (let key in programming) {
+//   console.log(key);
+// }
+// Using a loop, console.log all of the values in the programming object.
+// for (let key in programming) {
+//   console.log(programming[key]);
+// }
+
+
+// /Menu Items (MVP)///////////////////
+ 
+// const latte = {
+//   name: "Cafe Latte",
+//   price: 4,
+//   category: "Drinks",
+// };
+ 
+// const burger = { name: "Burger", price: 18, category: "Lunch" };
+// const breakfastBurrito = {
+//   name: "Breakfast Burrito",
+//   price: 16,
+//   category: "Breakfast",
+// };
+ 
+// /* Task 1a: write a function to return more menu items with the same format as the items above. */
+function createMenuItem(name, price, category) {
+  return {
+    name: name,
+    price: price,
+    category: category
+  };
+}
+
+console.log(createMenuItem("Buuz", 45, "Breakfast"));
+console.log(createMenuItem("Buuz", 45, "Breakfast"));
+console.log(createMenuItem("Buuz", 45, "Breakfast"));
+console.log(createMenuItem("Buuz", 45, "Breakfast"));
+console.log(createMenuItem("Coffee", 5, "Dinner"));
+// /* Task 1b: use your function to create 3 more menu items. You may add any items to the menu that you'd like */
+console.log(createMenuItem("Smoothie", 6, "Drinks"));
+console.log(createMenuItem("Cheeseburger", 15, "Lunch"));
+console.log(createMenuItem("Omelette", 10, "Breakfast"));
+// /* Task 2: You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to your burger object that automatically calculates price given a string as a parameter.
+const burger1 = {
+  name: "Burger",
+  price: 18,
+  category: "Lunch",
+  discount: function(type) {
+    if (type === "student" || type === "teacher") {
+      return this.price * 0.75; 
+    } else if (type === "public") {
+      return this.price * 0.90; 
+    } else {
+      return this.price; 
+    }
+  }
+};
+
+console.log(burger.discount("student"));  
+console.log(burger.discount("public"));   
+console.log(burger.discount("teacher"));  
+console.log(burger.discount("random"));   
+
+// console.log(burger.discount("student"));
+// console.log(burger.discount("public"));
+// c;
